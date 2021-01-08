c5fcb68c9a2ec27893de77691c5e1cebc58ea260
luchen5 <amadeus.luchen@gmail.com>
2021-01-08 15:56:59 +0700

    create all migration and model

---
+++ database/migrations/2021_01_08_085031_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('region');
+            $table->text('address');
+            $table->date('date');
+            $table->time('time');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
