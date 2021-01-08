bba6b8c37c8d865efa425aac3cfaa99c48e066e5
Mikhaya <mikhaya.jrh@gmail.com>
2021-01-08 13:31:26 +0700

    create base template

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap cdn -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Si Isi</title>
+</head>
+<body>
+    <div class="container">
+        @yield('body')
+    </div>
+</body>
+</html>
