450b6b60e35eb10ee0fd8d868fee2070d1ccfcf6
CIA66 <54312493+CIA66@users.noreply.github.com>
2021-01-08 19:02:50 +0700

    Update web.php
    buka return wiev welcomenya.

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 // login admin
 Route::get('/admin', 'ProductController@showAddProduct')->name('login');
