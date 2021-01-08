7c62a6b50f630cb968ae33f0d414663c0dd3d4fa
luchen5 <amadeus.luchen@gmail.com>
2021-01-08 16:18:02 +0700

    add stock into product table

--- database/migrations/2021_01_08_084817_create_products_table.php
+++ database/migrations/2021_01_08_084817_create_products_table.php
@@ -20,6 +20,7 @@ class CreateProductsTable extends Migration
             $table->text('description');
             $table->string('image');
             $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('stock');
             $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
