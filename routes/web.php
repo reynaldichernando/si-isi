cf837a4890d92aba35ea4b3c7c4f3c9fc9976396
luchen5 <amadeus.luchen@gmail.com>
2021-01-09 20:35:21 +0700

    fix auth undefined homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $auth = Auth::check();
+
+    return view('home', ['auth' => $auth]);
 });
 
 // page => produk
