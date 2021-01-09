563768d1b107e8f66d9f48ef3d75818b097399f6
Pilboi <Philip.kwandynata@binus.ac.id>
2021-01-09 17:16:28 +0700

    ini sudah ga homo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,19 +1,30 @@
-
-
-<link href="{{ asset('css/footer_header_styles.css') }}" rel="stylesheet">
+<!-- <link href="{{ asset('css/footer_header_styles.css') }}" rel="stylesheet">
 
 <div class="head">
     <div class="topnav">
         <div class="left">
-            <a href="HOMO"><img src="assets/other/logo.png" alt="HOMO TAK ADA"></a>
+            <a href="{{ url('/') }}"><img src="assets/other/logo.png" alt="HOMO TAK ADA"></a>
         </div>
 
         <div class="right">
-            <a href="#">SI ISI</a>
+            <a href="{{ url('/') }}">SI ISI</a>
             <a href="#">PANGGILAN KAMI</a>
             <a href="#">PRODUK</a>
         </div>
     </div>
 </div>
+<br> -->
 
-<br>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal">
+    <a href="{{ url('/') }}"><img src="assets/other/logo.png" alt="HOMO TAK ADA"></a>
+  </h5>
+  
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="{{ url('/') }}">SI ISI</a>
+    <a class="p-2 text-dark" href="#">PANGGILAN KAMI</a>
+    <a class="p-2 text-dark" href="#">PRODUK</a>
+  </nav>
+
+  <!-- <a class="btn btn-outline-primary" href="#">Sign up</a> -->
+</div>
