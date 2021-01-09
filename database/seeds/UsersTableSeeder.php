3190494d79f130642224edefa6ba61e9f54a46b4
Mikhaya <mikhaya.jrh@gmail.com>
2021-01-09 09:21:06 +0700

    revision database

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,18 +12,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
         DB::table('users')->insert([
-            // [
-            //     'name' => '',
-            //     'email' => '@gmail.com',
-            //     'password' => bcrypt(''),
-            // ],
             [
                 'name' => 'admin',
                 'email' => 'admin@gmail.com',
                 'password' => bcrypt('admin'),
-            ],
+            ]
         ]);
     }
 }
