370a68d7134458f9b983142c13ff44a9b434eecf
luchen5 <amadeus.luchen@gmail.com>
2021-01-09 20:30:28 +0700

    fix update transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,15 +9,10 @@ use Illuminate\Support\Facades\Auth;
 class TransactionController extends Controller
 {
     public function showTransaction(){
-<<<<<<< HEAD
+        $auth = Auth::check();
         $role = 'guest';
         if(Auth::check()) $role = 'admin';
         $transaction = Transaction::orderBy('created_at')->get();
-=======
-        $auth = Auth::check();
-
-        $transaction = Transaction::orderBy('date')->get();
->>>>>>> 5c4b28c6c4b43943a78e2f7600478d920d0b685d
         
         return view('admin.viewTransaction', [
             'auth' => $auth,
