8ca5ed8d4e846f45c30221fe6a77d813bb1df912
CIA66 <54312493+CIA66@users.noreply.github.com>
2021-01-09 16:17:40 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,11 +11,20 @@
 
 <!-- Pesan Sekarang dan Keunggulan -->
 <div class="card1">
-    <button class="button button5">Black</button>
+    <br><br>
+    <button class="button button5">PESAN SEKARANG</button>
+    <br><br>
+    
+    <div class="center1">
+        <h3>KEUNGGULAN</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
 </div>
 
 <!-- Cara Isi Ulang -->
+<br><br><br><br>
 <div class="container">
+    <h3 class="judul">CARA ISI ULANG</h3>
     <div class="leftcolumn">
         <img src="assets/other/home_bg.jpg" alt="Bawa kontainer bersih dan kering, lalu timbang" height="100%" width="100%">
         <h3>Bawa kontainer bersih dan kering, lalu timbang</h3>
