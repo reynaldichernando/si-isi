4f699bb0f4b95f746b18df158c264f4b1208eda4
Pilboi <Philip.kwandynata@binus.ac.id>
2021-01-09 18:57:59 +0700

    foot hao le

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
 <link href="{{ asset('css/footer_header_styles.css') }}" rel="stylesheet">
 
-<div styles= "align:center">
-    <a href="https://www.instagram.com/si.isi.id/"><img src="assets/other/logo.png" alt="HOMO TAK ADA" width="40" height="40"></a>
+<div class="foot">
+    <a href="https://www.instagram.com/si.isi.id/"><img src="assets/other/instagram.png" alt="HOMO " width="40" height="40"></a>
 </div>
