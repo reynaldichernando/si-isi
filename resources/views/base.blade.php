71b755194a74cc3aa87490b0ac4b346ddaaac298
Mikhaya <mikhaya.jrh@gmail.com>
2021-01-09 16:22:47 +0700

    revision bootstrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,8 @@
     <!-- bootstrap cdn -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <title>Si Isi</title>
 </head>
 <body>
