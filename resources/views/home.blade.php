abfad1dc9508d3d5c8245a0ac579dc087bb58ce0
CIA66 <54312493+CIA66@users.noreply.github.com>
2021-01-10 00:00:33 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,14 @@
         
         <div class="center1">
             <h3>KEUNGGULAN</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <ul class="a">
+                <li><h6>Mobile Refill Station</h6><p>Kurangi polusi plastik sekali pakai dengan mengisi ulang produk rumah tangga dan perawatan pribadi Anda di Mobile Refill Station kami.</p></li>
+                <li><h6>Schedule A House Call</h6><p>Kami membawa isi ulang ke pintu Anda! Jadwalkan panggilan ke rumah tanpa kontak untuk isi ulang sesuai keinginan Anda.</p></li>
+            </ul>
+            
+            
+            
+            
         </div>
     </div>
 
