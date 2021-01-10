8d94c4b35370e7c9d9c70472c43401fc6cea132f
Mikhaya <mikhaya.jrh@gmail.com>
2021-01-10 13:46:01 +0700

    create middleware

--- routes/web.php
+++ routes/web.php
@@ -14,37 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// page => si isi
 Route::get('/', function () {
     $auth = Auth::check();
 
     return view('home', ['auth' => $auth]);
 });
-
-// page => produk
-Route::get('/product', 'ProductController@viewProduct');
-
+// page => detail produk
+Route::get('/detail-product={id}', 'ProductController@showDetailProduct');
 // page => login admin
 Route::get('/admin', 'AuthController@showFormLogin')->name('login');
 Route::post('/admin', 'AuthController@login');
-// page => logout admin
-Route::get('/logout', 'AuthController@logout');
 
-// page => daftar produk
-Route::get('/product-list', 'ProductController@showProductList');
-// page => tambah produk
-Route::get('/add-product', 'ProductController@showAddProduct')->name('add_product');
-Route::post('/add-product', 'ProductController@addProduct');
-// page => ubah produk
-Route::get('/edit-product={id}', 'ProductController@showEditProduct')->name('edit_product');
-Route::post('/edit-product={id}', 'ProductController@editProduct');
-// page => hapus produk
-Route::get('/delete-product={id}', 'ProductController@deleteProduct');
-// page => pemesanan
-Route::get('/view-transaction', 'TransactionController@showTransaction');
+// only for guest
+Route::group(['middleware' => ['guest']], function() {
+    // page => panggilan rumah
+    Route::get('/home-call', 'HomecallController@showForm')->name('confirm_call');
+    Route::post('/home-call', 'HomecallController@addTransaction');
+    // page => produk
+    Route::get('/product', 'ProductController@viewProduct');
+});
 
-// page => detail produk
-Route::get('/detail-product={id}', 'ProductController@showDetailProduct');
-
-// page => panggilan rumah
-Route::get('/home-call', 'HomecallController@showForm')->name('confirm_call');
-Route::post('/home-call', 'HomecallController@addTransaction');
+// only for admin
+Route::group(['middleware' => ['auth']], function() {
+    // page => daftar produk
+    Route::get('/product-list', 'ProductController@showProductList');
+    // page => tambah produk
+    Route::get('/add-product', 'ProductController@showAddProduct')->name('add_product');
+    Route::post('/add-product', 'ProductController@addProduct');
+    // page => ubah produk
+    Route::get('/edit-product={id}', 'ProductController@showEditProduct')->name('edit_product');
+    Route::post('/edit-product={id}', 'ProductController@editProduct');
+    // page => hapus produk
+    Route::get('/delete-product={id}', 'ProductController@deleteProduct');
+    // page => pemesanan
+    Route::get('/view-transaction', 'TransactionController@showTransaction');
+    // page => logout admin
+    Route::get('/logout', 'AuthController@logout');
+});
