c008bdcc669951023ec994fe50c8a3aca3a77c78
Mikhaya <mikhaya.jrh@gmail.com>
2021-01-10 22:08:25 +0700

    revision login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,8 +13,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
+            <div class="col-md-6 offset-md-3">
                 <div class="card-login">
                     <div class="card-header">
                         <h3 class="text-center">Login</h3>
@@ -52,7 +51,6 @@
                     </form>
                 </div>
             </div>
-            <div class="col-md-3"></div>
         </div>
     </div>
 </body>
